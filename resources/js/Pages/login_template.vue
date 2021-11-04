00fc8f66a166e47eeae509d35117430e361f3047
gabrises <gvap120@hotmail.com>
2021-11-03 22:51:20 -0300

    Front login template

--- resources/js/Pages/login_template.vue
+++ resources/js/Pages/login_template.vue
@@ -1,46 +1,74 @@
 <template>
+    
     <Head title="Log in" />
 
-    <jet-authentication-card>
-        <template #logo>
-            <jet-authentication-card-logo />
-        </template>
+    <div class="flex flex-row justify-center">
 
-        <jet-validation-errors class="mb-4" />
-
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+        <div class="flex flex-row items-center">
+            <img src="https://candogeat.net/wp-content/uploads/2019/07/can-dog-eat-bananas.jpg" 
+            alt="auau banana"
+            class="md:visible invisible">
         </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
+        
+        <!--- Login container --->
+        <jet-authentication-card class="flex flex-row ">
+
+            <jet-validation-errors class="mb-4" />
+
+            <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                {{ status }}
             </div>
 
-            <div class="mt-4">
-                <jet-label for="password" value="Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
-            </div>
+            <p class="text-2xl mt-20 mb-7">
+                Entrar
+            </p>
 
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <jet-checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
+            <form @submit.prevent="submit">
+                    
+                <div>
+                    <!--- <jet-label for="email" value="Email" /> --->
+                    <jet-input id="email" placeholder="E-mail" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
-                </Link>
+                <div class="mt-4">
+                    <!--- <jet-label for="password" value="Password" /> --->
+                    <jet-input id="password" placeholder="Senha" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
+                </div>
 
-                <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </jet-button>
-            </div>
-        </form>
-    </jet-authentication-card>
+                <div class="justify-start">
+
+                    <jet-button class="mt-6 sm:w-1/4 w-2/6" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        <p class="sm:ml-3 ml-4">
+                            Login
+                        </p>
+                    </jet-button>
+
+                    <p class="mt-5">
+                        Não tem conta? 
+                        <a :href="route('register')">
+                                Registre-se
+                        </a>
+                    </p>
+
+                    <p class="mt-2">
+                        Esqueceu sua senha? 
+                        <a :href="route('password.request')">
+                            Clique aqui
+                        </a>
+                    </p>
+
+                    <p class="mt-20 mb-5">
+                        Sobre o 
+                        <a href="">
+                            Banana Chat
+                        </a>
+                    </p>
+
+                </div>
+            </form>
+        </jet-authentication-card>
+    </div>
 </template>
 
 <script>
