88e9c64353272fecfb44c6696e2653876ef311c9
eduardamirelly <mirelly.eduarda404@gmail.com>
2021-12-09 23:21:35 -0300

    update config/app.php and public/js/app.js

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://web-banana-chat.herokuapp.com/chat'),
 
     'asset_url' => env('ASSET_URL', null),
 
