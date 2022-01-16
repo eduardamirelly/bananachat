9eb935a29fc50b4146fd17d4627186dcdedb3e07
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-01-16 17:12:28 -0300

    refactoring register.blade.php

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -63,6 +63,4 @@
         </div>
     </div>
 
-    <!-- END LOGIN -->
-
 @endsection
--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.base')
 
-@section('title', 'Register')
+@section('title', 'Cadastro')
 
 @section('content')
 
     <!-- REGISTER -->
-
-    <div id="content-block" class="flex flex-row justify-center items-center bg-whi-yellow font-poppin h-screen font-poppins">
-
-        <div class="content flex flex-col justify-center py-6 px-8 w-5/6 lg:w-3/6 md:w-4/6 h-4/5 bg-bro-dark rounded-r-md rounded-l-md shadow-lg">
-
+    <div class="block-fluid container mx-auto min-w-min px-4 py-12 sm:px-8 flex flex-col justify-center items-center">
+        <div class="w-32 mb-8">
+            <img src="{{ asset('assets/logo.svg') }}" alt="logo">
+        </div>
+        <div class="bg-bro-dark w-full sm:w-11/12 md:w-9/12 xl:w-5/12 px-6 py-8 sm:p-10 rounded-sm shadow-md">
             <!-- Session Status -->
             <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -17,14 +17,15 @@
             <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
             <p class="font-semibold text-3xl text-whi-yellow">
-                Entrar
+                Cadastro
             </p>
             
-            <form method="POST" action="{{ route('register') }}">
+            <!-- FORM REGISTER -->
+            <form method="POST" action="{{ route('login') }}">
                 @csrf
-
+                
                 <div>
-                    <input id="name" placeholder="Usuário" :value="old('name')" type="text" name="name" class="mt-6 rounded-md block w-full bg-ye-medium text-whi-opaque placeholder-whi-opaque h-14 border-none focus:ring-whi-yellow" required autofocus autocomplete="name" />
+                    <input id="name" placeholder="Nickname" :value="old('name')" type="text" name="name" class="mt-6 rounded-md block w-full bg-ye-medium text-whi-opaque placeholder-whi-opaque h-14 border-none focus:ring-whi-yellow" required autofocus autocomplete="name" />
                 </div>
 
                 <div class="mt-4">
@@ -40,24 +41,27 @@
                 </div>
 
                 <div class="justify-start">
-                    <button class="mt-4 bg-bro-medium py-3 px-14 rounded-md hover:bg-gr-medium" type="submit">
+                    <button class="mt-4 bg-bro-medium py-3 px-12 xs:px-14 rounded-md hover:bg-gr-medium" type="submit">
                         <p class="text-whi-yellow font-semibold text-lg">
-                            Register
+                            Registrar
                         </p>
                     </button>
+                    <p class="mt-6 text-whi-yellow font-regular text-sm">
+                        Já tem conta? 
+                        <a class="text-whi-opaque" href="{{ route('login') }}">
+                            Login
+                        </a>
+                    </p>
                 </div>
-
             </form>
+
             <p class="mt-10 text-whi-yellow text-sm">
                 Sobre o 
-                <a class="text-whi-opaque hover:text-gr-light" href="#">
+                <a class="text-whi-opaque hover:text-gr-light" href="{{ route('index') }}">
                     Banana Chat
                 </a>
             </p>
         </div>
-        
     </div>
-
-    <!-- END REGISTER -->
 
 @endsection
