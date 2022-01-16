96ffb949ba0931b57189d1486f13cddb63a27a16
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-01-16 20:38:15 -0300

    config mail.php

--- config/mail.php
+++ config/mail.php
@@ -92,7 +92,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'bananachatsystem@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'BananaChat'),
     ],
 
