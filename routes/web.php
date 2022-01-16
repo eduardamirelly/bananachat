6e178fb1e88596878da0b8ace4fa1ee4b4245cde
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-01-16 10:20:49 -0300

    add route in banana-brand

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.index');
-});
+})->name('index');
 
 Route::get('/tr', function () {
     return view('pages/login');
