957304ae25fb28fd3fb285a2718a3cbbfa21ac49
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-01-16 20:40:51 -0300

    config mail.php

--- config/mail.php
+++ config/mail.php
@@ -38,7 +38,7 @@ return [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 465),
-            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
