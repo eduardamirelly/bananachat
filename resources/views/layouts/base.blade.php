236e623cde91771cfa288b46d32d009701bbce8f
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-01-16 09:53:05 -0300

    clean code

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>BananaChat | @yield('title')</title>
-    <link rel="icon" href="{{ asset('/assets/logo-bananachat.png') }}">
-    <link rel="stylesheet" href="{{ asset('/css/main.css') }}">
+    <link rel="icon" href="{{ asset('assets/logo-bananachat.png') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
