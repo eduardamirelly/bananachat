2fd83194f9c9f87046c64167ae59e43108c6d33d
JVJVJVJVJVJV <pinheiro.v@escolar.ifrn.edu.br>
2022-01-20 20:17:05 -0300

    create migrations

---
+++ database/migrations/2022_01_20_194027_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->longText('content');
+            $table->unsignedBigInteger('sender_id');
+            $table->string('sender_name');
+            //id chat vinculado (fk)
+            $table->timestamps();
+
+            $table->foreign('sender_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
